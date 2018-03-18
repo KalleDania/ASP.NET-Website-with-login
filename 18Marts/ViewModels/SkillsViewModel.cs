7b6e94bfdc830e9bfd2e69da4e836ded08a9137a
Kasper Nielsen <kallenielsen87@live.dk>
2018-03-18 17:19:56 +0000

    a

---
+++ 18Marts/ViewModels/SkillsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eksempel_1.ViewModels
+{
+    public class SkillsViewModel
+    {
+    }
+}
