5b30b2dd8e4f9754323c3eb24e4268e45ccce755
Kasper Nielsen <kallenielsen87@live.dk>
2018-03-18 19:39:43 +0000

    d

--- 18Marts/Controllers/HomeController.cs
+++ 18Marts/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace _18Marts.Controllers
             return View();
         }
 
-        public IActionResult Details(int id) // Uuuhuuuuuh baby!dfg
+        public IActionResult Details(int id) // Uuuhuuuuuh baby!dfgasd
         {
             var model = videos.Get(id);
 
